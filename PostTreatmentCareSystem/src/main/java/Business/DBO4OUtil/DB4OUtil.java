7ede5dd52afcf2bbe8e209193365ba487dee0db0
Samiksha <gupta.samik@northeastern.edu>
2024-11-27 12:28:40 -0500

    Creating packages

---
+++ PostTreatmentCareSystem/src/main/java/Business/DBO4OUtil/DB4OUtil.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.DBO4OUtil;
+
+/**
+ *
+ * @author samik
+ */
+public class DB4OUtil {
+    
+}
