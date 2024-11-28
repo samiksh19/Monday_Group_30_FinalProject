264e344316135f803c34015b5fb7103e20e2e0af
DaleVAlphonso <alphonso.d@northeastern.edu>
2024-11-27 20:39:52 -0500

    Added Business class

---
+++ PostTreatmentCareSystem/src/main/java/Business/Business.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Roles.Role;
+import Business.Roles.AdminRole;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author dalea
+ */
+public class Business {
+    
+    private static Business business;
+    private ArrayList<Network> networkList;
+    
+    public static Business getInstance(){
+        if(business==null){
+            business=new Business();
+        }
+        return business;
+    }
+    public Network createAndAddNetwork(){
+        Network network=new Network();
+        networkList.add(network);
+        return network;
+    }
+//    @Override
+//    public ArrayList<Role> getSupportedRole() {
+//        ArrayList<Role> roleList=new ArrayList<Role>();
+//        roleList.add(new AdminRole());
+//        return roleList;
+//    }
+//    
+//    private Business(){
+//        super(null);
+//        networkList=new ArrayList<Network>();
+//    }
+     public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public void setNetworkList(ArrayList<Network> networkList) {
+        this.networkList = networkList;
+    }
+    
+    
+}
