1d96fcf1fef39e26da503747486176a7821e5e89
Samiksha <gupta.samik@northeastern.edu>
2024-12-07 17:02:13 -0500

    Logic for managing Pharmacy

--- PostHealthCareSystem/PostRecoverySystem/src/Business/Pharmaceuticals/Pharmaceutical.java
+++ PostHealthCareSystem/PostRecoverySystem/src/Business/Pharmaceuticals/Pharmaceutical.java
@@ -4,10 +4,81 @@
  */
 package Business.Pharmaceuticals;
 
+import Business.Porter.Porter;
+import Business.Role.SysAdminRole;
+import Business.UserAccount.UserAccount;
+
 /**
  *
  * @author samik
  */
-public class Pharmaceutical {
-    
+public class Pharmaceutical extends UserAccount{
+    private String orderId;
+    private SysAdminRole systemAdmin;
+    private Porter deliveryMan;
+    private String pharmaceuticalName;
+    private Inventory inventory;
+    private String address;
+    private String contact;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public SysAdminRole getSystemAdmin() {
+        return systemAdmin;
+    }
+
+    public void setSystemAdmin(SysAdminRole systemAdmin) {
+        this.systemAdmin = systemAdmin;
+    }
+
+    public Porter getDeliveryMan() {
+        return deliveryMan;
+    }
+
+    public void setDeliveryMan(Porter deliveryMan) {
+        this.deliveryMan = deliveryMan;
+    }
+
+    public String getPharmaceuticalName() {
+        return pharmaceuticalName;
+    }
+
+    public void setPharmaceuticalName(String pharmaceuticalName) {
+        this.pharmaceuticalName = pharmaceuticalName;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public String toString() {
+        return pharmaceuticalName;
+    }
 }
