609103e586b9664c38e47a67f2ab9a6d4462c617
pradhanprac <pradhan.prac@northeastern.edu>
2024-12-07 17:07:32 -0500

    Doctor Landing page and WorkRequest Page

--- PostHealthCareSystem/PostRecoverySystem/src/Business/Pharmaceutical/Medicines.java
+++ PostHealthCareSystem/PostRecoverySystem/src/Business/Pharmaceutical/Medicines.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Business.Pharmaceuticals;
+package Business.Pharmaceutical;
 
 /**
  *
  * @author samik
  */
-public class Pharmaceutical {
+public class Medicines {
     
 }
---
+++ PostHealthCareSystem/PostRecoverySystem/src/Business/Pharmaceutical/Pharmaceutical.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Pharmaceutical;
+
+import Business.Porter.Porter;
+import Business.Role.SysAdminRole;
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author samik
+ */
+public class Pharmaceutical extends UserAccount {
+    
+     private String orderId;
+    private SysAdminRole systemAdmin;
+    private Porter deliveryMan;
+    private String pharmaceuticalName;
+    private Inventory inventory;
+    private String address;
+    private String contact;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public SysAdminRole getSystemAdmin() {
+        return systemAdmin;
+    }
+
+    public void setSystemAdmin(SysAdminRole systemAdmin) {
+        this.systemAdmin = systemAdmin;
+    }
+
+    public Porter getDeliveryMan() {
+        return deliveryMan;
+    }
+
+    public void setDeliveryMan(Porter deliveryMan) {
+        this.deliveryMan = deliveryMan;
+    }
+
+    public String getPharmaceuticalName() {
+        return pharmaceuticalName;
+    }
+
+    public void setPharmaceuticalName(String pharmaceuticalName) {
+        this.pharmaceuticalName = pharmaceuticalName;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public String toString() {
+        return pharmaceuticalName;
+    }
+    
+}
