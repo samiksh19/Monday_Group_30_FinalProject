14c588a71064819b1e825969215f40be4b6bacff
pradhanprac <pradhan.prac@northeastern.edu>
2024-12-08 18:24:38 -0500

    Pharmacy Implementation

--- PostHealthCareSystem/PostRecoverySystem/src/ui/PharmacyAdminRole/CreateItem.java
+++ PostHealthCareSystem/PostRecoverySystem/src/ui/PharmacyAdminRole/CreateItem.java
@@ -4,6 +4,20 @@
  */
 package ui.PharmacyAdminRole;
 
+import Business.Business;
+import Business.Pharmacy.Pharmacy;
+import Business.UserAccount.UserAccount;
+import Business.Pharmacy.PharmacyInventory;
+import Business.Pharmacy.PharmacyMedicine;
+import java.awt.CardLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author prach
@@ -13,8 +27,56 @@ public class CreateItem extends javax.swing.JPanel {
     /**
      * Creates new form CreateItem
      */
-    public CreateItem() {
+    JPanel userProcessContainer;
+    Business business;
+    UserAccount userAccount;
+    Pharmacy pharmacy;
+    private DefaultTableModel defaultTableModel;
+    private List<PharmacyInventory> itemQuantityList = new ArrayList<>();
+    public CreateItem(JPanel userProcessContainer, UserAccount userAccount, Business business) {
         initComponents();
+        initListners();
+        this.userProcessContainer = userProcessContainer;
+        this.business = business;
+        this.userAccount = userAccount;
+        defaultTableModel = (DefaultTableModel) tblMenu.getModel();
+        pharmacy = (Pharmacy) userAccount;
+        populateTable(); 
+    }
+    private void populateTable() {
+        PharmacyInventory menu = pharmacy.getMenu();
+        DefaultTableModel model = (DefaultTableModel) tblMenu.getModel();
+        model.setRowCount(0);
+        if(menu != null){
+        for (PharmacyMedicine item : menu.getItemList()) {
+
+            Object[] row = new Object[3];
+            row[0] = item;
+            row[1] = item.getPrice();
+            row[2] = item.getQty();
+            model.addRow(row);
+        }
+        }
+    }
+
+    private void display(PharmacyMedicine item) {
+        itemName.setText(item.getName());
+        itemPrice.setText(String.valueOf(item.getPrice()));
+        itemQty.setText(String.valueOf(item.getQty()));
+
+    }
+    private void initListners() {
+        tblMenu.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent event) {
+                int selectedRow = tblMenu.getSelectedRow();
+                if (selectedRow >= 0) {
+                    PharmacyMedicine item = (PharmacyMedicine) tblMenu.getValueAt(selectedRow, 0);
+                    if (item != null) {
+                        display(item);
+                    }
+                }
+            }
+        });
     }
 
     /**
@@ -26,19 +88,269 @@ public class CreateItem extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblMenu = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        itemName = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        itemPrice = new javax.swing.JTextField();
+        btnAddItem = new javax.swing.JButton();
+        btnUpdateItem = new javax.swing.JButton();
+        btnBack2 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        itemQty = new javax.swing.JTextField();
+
+        jPanel1.setBackground(new java.awt.Color(0, 102, 102));
+
+        jLabel1.setFont(new java.awt.Font("Garamond", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("MANAGE INVENTORY");
+
+        tblMenu.setForeground(new java.awt.Color(204, 255, 204));
+        tblMenu.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ITEM NAME", "ITEM PRICE", "ITEM QUANTITY"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tblMenu);
+
+        jLabel2.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Item Name");
+
+        itemName.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+
+        jLabel3.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Item Price");
+
+        itemPrice.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+
+        btnAddItem.setBackground(new java.awt.Color(255, 255, 204));
+        btnAddItem.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        btnAddItem.setText("Add Item");
+        btnAddItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddItemActionPerformed(evt);
+            }
+        });
+
+        btnUpdateItem.setBackground(new java.awt.Color(255, 255, 204));
+        btnUpdateItem.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        btnUpdateItem.setText("Update");
+        btnUpdateItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateItemActionPerformed(evt);
+            }
+        });
+
+        btnBack2.setBackground(new java.awt.Color(255, 255, 204));
+        btnBack2.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        btnBack2.setText("Back");
+        btnBack2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBack2ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Item Quantity");
+
+        itemQty.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnBack2, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(65, 65, 65)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 816, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 816, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(192, 192, 192)
+                                .addComponent(jLabel2)
+                                .addGap(36, 36, 36)
+                                .addComponent(itemName, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(200, 200, 200)
+                                .addComponent(jLabel3)
+                                .addGap(35, 35, 35)
+                                .addComponent(itemPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(172, 172, 172)
+                                .addComponent(jLabel4)
+                                .addGap(29, 29, 29)
+                                .addComponent(itemQty, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(225, 225, 225)
+                                .addComponent(btnAddItem, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(10, 10, 10)
+                                .addComponent(btnUpdateItem, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(313, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnBack2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(8, 8, 8)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(3, 3, 3)
+                        .addComponent(jLabel2))
+                    .addComponent(itemName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(13, 13, 13)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(3, 3, 3)
+                        .addComponent(jLabel3))
+                    .addComponent(itemPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(13, 13, 13)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(3, 3, 3)
+                        .addComponent(jLabel4))
+                    .addComponent(itemQty, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(16, 16, 16)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAddItem, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnUpdateItem, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(333, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1200, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 880, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnAddItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddItemActionPerformed
+        if (itemName.getText().isEmpty() || itemPrice.getText().isEmpty() || itemQty.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Enter all fields");
+            return;
+        }
+        if(pharmacy.getMenu() != null){
+            for (PharmacyMedicine item : pharmacy.getMenu().getItemList()) {
+                if (itemName.getText().equals(item.getName())) {
+                    JOptionPane.showMessageDialog(null, "Duplicate Items");
+                    return;
+                }
+            }
+        }
+        double price = 0.0;
+        int qty = 0;
+        try {
+            price = Double.valueOf(itemPrice.getText());
+            qty = Integer.valueOf(itemQty.getText());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Enter a valid number");
+            return;
+        }
+
+        PharmacyMedicine item = new PharmacyMedicine(itemName.getText(), Double.valueOf(itemPrice.getText()),Integer.valueOf(itemQty.getText()));
+
+        if(pharmacy.getMenu() != null){
+            pharmacy.getMenu().addItem(item);
+            populateTable();
+            itemName.setText("");
+            itemPrice.setText("");
+            itemQty.setText("");
+        } else{
+            PharmacyInventory menu = new PharmacyInventory();
+            menu.addItem(item);
+            pharmacy.setMenu(menu);
+            populateTable();
+            itemName.setText("");
+            itemPrice.setText("");
+            itemQty.setText("");
+        }
+    }//GEN-LAST:event_btnAddItemActionPerformed
+
+    private void btnUpdateItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdateItemActionPerformed
+        int selectedRow = tblMenu.getSelectedRow();
+        if (selectedRow >= 0) {
+
+            PharmacyMedicine item = (PharmacyMedicine) tblMenu.getValueAt(selectedRow, 0);
+            item.setName(itemName.getText());
+            item.setPrice(Double.valueOf(itemPrice.getText()));
+            item.setQty(Integer.valueOf(itemQty.getText()));
+            populateTable();
+            itemName.setText("");
+            itemPrice.setText("");
+            itemQty.setText("");
+
+        } else {
+            JOptionPane.showMessageDialog(null,"Pick a row");
+        }
+    }//GEN-LAST:event_btnUpdateItemActionPerformed
+
+    private void btnBack2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBack2ActionPerformed
+        PharmacyAdminWorkArea adminWorkAreaJPanel = new PharmacyAdminWorkArea(userProcessContainer,userAccount, business);
+        userProcessContainer.add("AdminWorkAreaJPanel", adminWorkAreaJPanel);
+        CardLayout layout = (CardLayout)userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_btnBack2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddItem;
+    private javax.swing.JButton btnBack2;
+    private javax.swing.JButton btnUpdateItem;
+    private javax.swing.JTextField itemName;
+    private javax.swing.JTextField itemPrice;
+    private javax.swing.JTextField itemQty;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblMenu;
     // End of variables declaration//GEN-END:variables
 }
--- PostHealthCareSystem/PostRecoverySystem/src/ui/PharmacyAdminRole/PharmacyAdminWorkArea.java
+++ PostHealthCareSystem/PostRecoverySystem/src/ui/PharmacyAdminRole/PharmacyAdminWorkArea.java
@@ -6,6 +6,7 @@ package ui.PharmacyAdminRole;
 
 import Business.Business;
 import Business.UserAccount.UserAccount;
+import java.awt.CardLayout;
 import javax.swing.JPanel;
 
 /**
@@ -26,7 +27,27 @@ public class PharmacyAdminWorkArea extends javax.swing.JPanel {
         this.business=business;
         this.userAccount = userAccount; 
     }
-
+    private void manageOrders() {
+        ManageOrders viewOrderDetails = new ManageOrders(userProcessContainer,business,userAccount);
+        userProcessContainer.add("ViewOrderDetails",viewOrderDetails);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }
+    
+    private void manageMenu() {
+        CreateItem createMenuItemJPanel = new CreateItem(userProcessContainer,userAccount,business);
+        userProcessContainer.add("CreateMenuItemJPanel",createMenuItemJPanel);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }
+    
+    private void manageInfo() {
+        ManagePharmacies updateRestaurantInfoJPanel = new ManagePharmacies(userProcessContainer,business, userAccount);
+        userProcessContainer.add("UpdateRestaurantInfoJPanel",updateRestaurantInfoJPanel);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }
+  
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -35,20 +56,98 @@ public class PharmacyAdminWorkArea extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jPanel1 = new javax.swing.JPanel();
+        userJButton = new javax.swing.JButton();
+        manageEmployeeJButton = new javax.swing.JButton();
+        manageOrganizationJButton = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        jPanel1.setBackground(new java.awt.Color(0, 102, 102));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        userJButton.setBackground(new java.awt.Color(255, 255, 204));
+        userJButton.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        userJButton.setText("Manage Pharmacy Information");
+        userJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userJButtonActionPerformed(evt);
+            }
+        });
+        jPanel1.add(userJButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 380, 299, 48));
+
+        manageEmployeeJButton.setBackground(new java.awt.Color(255, 255, 204));
+        manageEmployeeJButton.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        manageEmployeeJButton.setText("Manage Inventory");
+        manageEmployeeJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                manageEmployeeJButtonActionPerformed(evt);
+            }
+        });
+        jPanel1.add(manageEmployeeJButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 200, 315, 47));
+
+        manageOrganizationJButton.setBackground(new java.awt.Color(255, 255, 204));
+        manageOrganizationJButton.setFont(new java.awt.Font("Garamond", 1, 18)); // NOI18N
+        manageOrganizationJButton.setText("Manage Orders");
+        manageOrganizationJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                manageOrganizationJButtonActionPerformed(evt);
+            }
+        });
+        jPanel1.add(manageOrganizationJButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 200, 317, 49));
+
+        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("WELCOME PHARMACY ADMIN !");
+        jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 70, -1, 60));
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/UI/PharmacyAdminRole/pharmacy.jpeg"))); // NOI18N
+        jLabel3.setMaximumSize(new java.awt.Dimension(8000, 4929));
+        jLabel3.setPreferredSize(new java.awt.Dimension(8000, 4929));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 0, 0));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1104, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1104, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 667, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 661, Short.MAX_VALUE)
+                    .addContainerGap()))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void userJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userJButtonActionPerformed
+        manageInfo();
+    }//GEN-LAST:event_userJButtonActionPerformed
+
+    private void manageEmployeeJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manageEmployeeJButtonActionPerformed
+        manageMenu();
+    }//GEN-LAST:event_manageEmployeeJButtonActionPerformed
+
+    private void manageOrganizationJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manageOrganizationJButtonActionPerformed
+        manageOrders();
+    }//GEN-LAST:event_manageOrganizationJButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton manageEmployeeJButton;
+    private javax.swing.JButton manageOrganizationJButton;
+    private javax.swing.JButton userJButton;
     // End of variables declaration//GEN-END:variables
 }
