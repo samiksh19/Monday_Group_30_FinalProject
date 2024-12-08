fde8a4fb98a96b6ff39bfc04bbbc6540b206462b
pradhanprac <pradhan.prac@northeastern.edu>
2024-12-07 20:18:54 -0500

    Resolving conflicts

--- PostHealthCareSystem/PostRecoverySystem/src/Business/ConfigureASystem.java
+++ PostHealthCareSystem/PostRecoverySystem/src/Business/ConfigureASystem.java
@@ -27,7 +27,7 @@ public class ConfigureASystem {
        Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
         
        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SysAdminRole());
-        
+        //
         return system;
     }
 }
