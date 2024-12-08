8681a5f9a495b7273e5d2ef34067283ac3824454
DaleVAlphonso <alphonso.d@northeastern.edu>
2024-12-07 21:04:16 -0500

    Resolving conflicts

--- PostHealthCareSystem/PostRecoverySystem/src/ui/AccountantRole/AccountantWorkArea.java
+++ PostHealthCareSystem/PostRecoverySystem/src/ui/AccountantRole/AccountantWorkArea.java
@@ -6,9 +6,14 @@ package ui.AccountantRole;
 
 import Business.Business;
 import Business.UserAccount.UserAccount;
+import Business.WorkRequest.LabWorkRequest;
 import Business.WorkRequest.WorkRequest;
+import java.awt.CardLayout;
 import java.util.List;
 import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -25,11 +30,46 @@ public class AccountantWorkArea extends javax.swing.JPanel {
      */
     public AccountantWorkArea(JPanel userProcessContainer, UserAccount userAccount, Business Business) {
         initComponents();
-//        creatingListenerForDelInfo();
+        creatingListenerForDelInfo();
         this.userProcessContainer = userProcessContainer;
         this.userAccount = userAccount;
         this.Business = Business;
-//        fillDelRqTable();
+        fillDelRqTable();
+    }
+    private void fillDelRqTable(){
+        DefaultTableModel model = (DefaultTableModel) tblDeliveryManWorkRequest.getModel();
+        model.setRowCount(0);
+        workRequestList = Business.getWorkQueue().getWorkRequestListAccount(userAccount);
+        for (WorkRequest request : workRequestList) {
+            Object[] row = new Object[tblDeliveryManWorkRequest.getColumnCount()];
+            row[0] = request;
+            row[1] = request.getReception();
+            row[2] = request.getPatient();
+            row[3] = request.getStatus();
+            row[4] = request.getBill();
+            model.addRow(row);
+        }
+        }
+
+        private void creatingListenerForDelInfo() {
+        tblDeliveryManWorkRequest.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent event) {
+                int selectedRow = tblDeliveryManWorkRequest.getSelectedRow();
+                if (selectedRow >= 0) {
+                    WorkRequest request = (WorkRequest) tblDeliveryManWorkRequest.getValueAt(selectedRow, 0);
+                    if (request instanceof LabWorkRequest) {
+                        LabWorkRequest orderWorkRequest = (LabWorkRequest) tblDeliveryManWorkRequest.getValueAt(selectedRow, 0);
+                        if (orderWorkRequest != null) {
+                           ProcessAccountingRequests processOrderJPanel = new ProcessAccountingRequests(userProcessContainer,Business,userAccount,orderWorkRequest);
+                           userProcessContainer.add("ProcessOrderJPanel", processOrderJPanel);
+                           CardLayout layout = (CardLayout)userProcessContainer.getLayout();
+                           layout.next(userProcessContainer);
+                        }
+                    }
+
+                }
+            }
+        });
     }
 
    
@@ -43,19 +83,77 @@ public class AccountantWorkArea extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tblDeliveryManWorkRequest = new javax.swing.JTable();
+
+        setBackground(new java.awt.Color(0, 102, 102));
+
+        jLabel1.setFont(new java.awt.Font("Garamond", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Billing Details");
+
+        tblDeliveryManWorkRequest.setBackground(new java.awt.Color(204, 255, 204));
+        tblDeliveryManWorkRequest.setFont(new java.awt.Font("Garamond", 0, 14)); // NOI18N
+        tblDeliveryManWorkRequest.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "MESSAGE", "SENDER", "RECEIVER", "STATUS", "BILL"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(tblDeliveryManWorkRequest);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1028, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 1028, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 757, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jLabel1)
+                    .addGap(18, 18, 18)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 698, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable tblDeliveryManWorkRequest;
     // End of variables declaration//GEN-END:variables
 }
